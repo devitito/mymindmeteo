2a35ac87aa73e0b4a18fb424fb40fe624b55273f
devitito <julien_desbois@hotmail.com>
2014-05-31 19:14:21 +0200

    apache configuration cookbook

---
+++ resources/cookbooks/apache/metadata.rb
@@ -0,0 +1,8 @@
+name             'apache'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures apache'
+long_description 'Installs/Configures apache'
+version          '0.1.0'
+
---
+++ resources/cookbooks/apache/recipes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+template "/etc/apache2/sites-available/vagrant" do
+  source "vhost.erb"
+end
+
+file "/etc/apache2/sites-enabled/000-default" do
+  action :delete
+end
+
+link "/etc/apache2/sites-enabled/000-vagrant" do
+  to "/etc/apache2/sites-available/vagrant"
+  action :create
+end
+
+execute "Allowing apache to access vagrant files" do
+  command "adduser www-data vagrant"
+  action :run
+end
+
+service "apache2" do
+  action :restart
+end
