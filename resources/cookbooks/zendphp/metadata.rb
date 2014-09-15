c7bf1927179f3fb7b1672f80008fec838f73c50e
devitito <julien_desbois@hotmail.com>
2014-09-15 10:10:53 +0200

    support for ubuntu 14.04 and zs 7.0

---
+++ resources/cookbooks/zendphp/metadata.rb
@@ -0,0 +1,11 @@
+name             'zendphp'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures zendphp'
+long_description 'Installs/Configures zendphp'
+version          '0.1.0'
+
+depends "apt"
+depends "build-essential"
+depends "conf"
---
+++ resources/cookbooks/zendphp/recipes/default.rb
@@ -0,0 +1,32 @@
+#
+# Cookbook Name:: zendphp
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+include_recipe "zendphp::repository"
+
+package "zend-server-php-5.5"
+  conf_plain_file '/etc/profile' do
+  pattern /\/usr\/local\/zend\/bin/
+  new_line 'PATH=$PATH:/usr/local/zend/bin'
+  action :insert_if_no_match
+end
+
+conf_plain_file '/etc/profile' do
+  pattern /\/usr\/local\/zend\/lib/
+  new_line 'LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/zend/lib'
+  action :insert_if_no_match
+end
+
+link "/usr/bin/php" do
+  to "/usr/local/zend/bin/php"
+  action :create
+end
+
+service "apache2" do
+  action :restart
+end
