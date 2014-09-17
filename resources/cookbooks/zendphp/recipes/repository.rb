545315727d4599ff14f4742bbbd0305a68049451
devitito <julien_desbois@hotmail.com>
2014-09-17 11:38:04 +0200

    configuration for ubuntu 1404 with zs6.3

--- resources/cookbooks/zendphp/recipes/repository.rb
+++ resources/cookbooks/zendphp/recipes/repository.rb
@@ -2,7 +2,7 @@ include_recipe "apt"
 
 # add the Zend Server repo
 apt_repository "zend-server" do
-  uri "http://repos.zend.com/zend-server/7.0/deb_apache2.4"
+  uri "http://repos.zend.com/zend-server/6.3/deb_apache2.4"
   distribution "server"
   components ["non-free"]
   key "http://repos.zend.com/zend.key"
