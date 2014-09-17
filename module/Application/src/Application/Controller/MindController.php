4d48de4f9552d82a3d859afa78e6a5df6e46e574
devitito <julien_desbois@hotmail.com>
2014-09-17 17:44:20 +0200

    dashboard maquet

--- module/Application/src/Application/Controller/MindController.php
+++ module/Application/src/Application/Controller/MindController.php
@@ -4,13 +4,23 @@ namespace Application\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Application\Exception;
+use Zend\View\Model\ViewModel;
+use Application\Entity\Mind;
+use Application\Services\MindManager;
 
 class MindController extends AbstractActionController
 {
 	public function dashboardAction()
 	{
 		//check the mind exist
+		$mindManager = $this->getServiceLocator()->get('mind-manager');
+		$mind = $mindManager->getMind(['name' => $this->getEvent()->getRouteMatch()->getParam('mindname')]);
+		if (!$mind) {
+			$viewModel = new ViewModel();
+			$viewModel->setTemplate('error/404');
+			return $viewModel;
+		}
 		
-		//return $this->redirect()->toRoute('dashboard');
+		return ['mind' => $mind];
 	}
 }
