d22a054762f27f173503ae14a348b22e0284be77
devitito <julien_desbois@hotmail.com>
2014-09-30 16:24:38 +0200

    update model : samples

---
+++ module/Application/src/Application/Entity/Repository/SampleRepository.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * MindMeteo
+ * 
+ */
+
+namespace Application\Entity\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * SampleRepository
+ *
+ * Repository class to extend Doctrine ORM functions 
+ *
+ */
+class SampleRepository extends EntityRepository
+{
+	
+}
--- resources/cookbooks/mymeteodb/files/default/mymeteo-create.sql
+++ resources/cookbooks/mymeteodb/files/default/mymeteo-create.sql
@@ -19,12 +19,23 @@ CREATE TABLE mymeteo.minds(
 CREATE TABLE mymeteo.sensors (
   id varchar(32) COLLATE utf8_unicode_ci NOT NULL,
   label varchar(128) COLLATE utf8_unicode_ci NOT NULL,
-  img varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  img varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
   topic varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   meteologist varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+CREATE TABLE mymeteo.samples (
+  id varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  sensor_id varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  label varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  img varchar(128) COLLATE utf8_unicode_ci DEFAULT NULL,
+  topic varchar(64) COLLATE utf8_unicode_ci NOT NULL,
+  value int(11) NOT NULL,
+  PRIMARY KEY (id),
+  KEY IDX_19925777A247991F (sensor_id),
+  CONSTRAINT FK_19925777A247991F FOREIGN KEY (sensor_id) REFERENCES sensors (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO mymeteo.minds ( id, name, email, password, joindate, timezone ) VALUES ( '541953917c568', 'demo', 'demo@mindmeteo.com', '$2y$10$yPTMpSdcK5kdJqV1aHUpCej1gaGB81iwc8hKfXNOTLHuvS/xvdaGi', CURDATE(), 'Europe/Paris');
 
