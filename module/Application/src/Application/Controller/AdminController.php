40461122ace6cf07e9e43827cb841fa58f427113
devitito <julien_desbois@hotmail.com>
2014-10-21 11:52:18 +0200

    admin layout

--- module/Application/src/Application/Controller/AdminController.php
+++ module/Application/src/Application/Controller/AdminController.php
@@ -9,26 +9,31 @@ class AdminController extends AbstractActionController
 {
 	public function dashboardAction()
 	{
+		$this->layout('layout/admin');
 		return new ViewModel();
 	}
 	
 	public function mindsAction()
 	{
+		$this->layout('layout/admin');
 		return new ViewModel();
 	}
 	
 	public function statsAction()
 	{
+		$this->layout('layout/admin');
 		return new ViewModel();
 	}
 	
 	public function sensorsAction()
 	{
+		$this->layout('layout/admin');
 		return new ViewModel();
 	}
 	
 	public function createMindAction()
 	{
+		$this->layout('layout/admin');
 		return new ViewModel();
 	}
 }
