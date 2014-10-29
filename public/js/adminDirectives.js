522c703d7d7ebacfa1aca3e6ca6cd213da35a443
devitito <julien_desbois@hotmail.com>
2014-10-29 12:45:51 +0100

    equalsTo directive password and confirmpassword identity

--- public/js/adminDirectives.js
+++ public/js/adminDirectives.js
@@ -19,3 +19,29 @@ adminDirectives.directive('ngUnique', ['$http', function ($http) {
 	}
 }
 ]);
+
+adminDirectives.directive('equalsTo', [function () {
+    /*
+     * <input type="password" ng-model="Password" />
+     * <input type="password" ng-model="ConfirmPassword" equals-to="Password" />
+     */
+    return {
+        restrict: 'A', // S'utilise uniquement en tant qu'attribut
+        scope: true,
+        require: 'ngModel',
+        link: function (scope, elem, attrs, control) {
+            var check = function () {
+                //Valeur du champs courant
+                var v1 = scope.$eval(attrs.ngModel); // attrs.ngModel = "confirmedpassword"
+ 
+                //valeur du champ à comparer
+                var v2 = scope.$eval(attrs.equalsTo).$viewValue; // attrs.equalsTo = "Password"
+                return v1 == v2;
+            };
+            scope.$watch(check, function (isValid) {
+                // Défini si le champ est valide
+                control.$setValidity("equalsTo", isValid);
+            });
+        }
+    };
+}]);
