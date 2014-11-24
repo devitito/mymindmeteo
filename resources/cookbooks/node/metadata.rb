3302e1e084e9cd78b91affa60aec130bdc759eff
devitito <julien_desbois@hotmail.com>
2014-11-24 17:44:21 +0100

    sails skeleton

---
+++ resources/cookbooks/node/metadata.rb
@@ -0,0 +1,9 @@
+name             'node'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures node'
+long_description 'Installs/Configures node'
+version          '0.1.0'
+
+depends "apt"
---
+++ resources/cookbooks/node/recipes/default.rb
@@ -0,0 +1,44 @@
+#
+# Cookbook Name:: node
+# Install Node.js and friends
+# Recipe:: default
+#
+# Copyright (C) 2014 devitito
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "Compile and install python" do
+  command "apt-get install python-software-properties python g++ make --yes"
+  action :run
+end
+
+execute "add-apt-repository" do
+  command "apt-get install software-properties-common --yes"
+  action :run	
+end
+
+execute "Add the PPA Repository, which is recommended by Joyent (the maintainers of Node.js)" do
+  command "add-apt-repository ppa:chris-lea/node.js -y"
+  action :run
+end
+
+execute "Update the package list" do
+  command "apt-get update"
+  action :run
+end
+
+execute "Install node and npm" do
+  command "apt-get install nodejs nodejs-dev --yes"
+  action :run
+end
+
+execute "Install grunt and grunt-cli" do
+  command "npm install -g grunt-cli -y"
+  action :run
+end
+
+execute "Install Sails and Forever" do
+  command "npm -g install sails forever --yes"
+  action :run
+end
