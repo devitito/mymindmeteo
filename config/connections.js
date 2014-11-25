a0b74c314a6500622f5550decbd47199a9f432fe
devitito <julien_desbois@hotmail.com>
2014-11-25 17:22:46 +0100

    mysql connection

--- config/connections.js
+++ config/connections.js
@@ -40,12 +40,15 @@ module.exports.connections = {
   * Run: npm install sails-mysql                                             *
   *                                                                          *
   ***************************************************************************/
-  someMysqlServer: {
+  mindMeteoDb: {
     adapter: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    password: 'YOUR_MYSQL_PASSWORD',
-    database: 'YOUR_MYSQL_DB'
+    host: 'localhost',
+    port     : 3306,
+    user: 'mymeteo_app',
+    password: 'supersecret',
+    database: 'mymeteo',
+    
+    schema: true
   },
 
   /***************************************************************************
