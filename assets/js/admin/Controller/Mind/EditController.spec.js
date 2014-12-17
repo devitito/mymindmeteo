9ccabf349b96b589abb25a1d3487beea2807d210
devitito <julien_desbois@hotmail.com>
2014-12-17 11:25:41 +0100

    mind edit controller test

--- assets/js/admin/Controller/Mind/EditController.spec.js
+++ assets/js/admin/Controller/Mind/EditController.spec.js
@@ -7,6 +7,7 @@ describe("Mind.EditController", function() {
 	var flash;
 	var $controller;
 	var $scope;
+	var $rootScope;
 
 	beforeEach(function () {
 		module('adminControllers');
@@ -15,20 +16,23 @@ describe("Mind.EditController", function() {
 		module('ui.bootstrap');
 	});
 
-	beforeEach(inject(function(_$controller_, _flash_) {
+	beforeEach(inject(function(_$controller_, _$rootScope_, _flash_) {
 		$controller = _$controller_;
 		$scope = {};
+		$rootScope = _$rootScope_;
 		flash = _flash_;
-
-		spyOn(flash, 'setMessage');
 	}));
 
-	it("Should push an error message in flash messenger if $scope.mind is not an object", function() {
-			EditController = $controller('EditMindCtrl', { $scope: $scope, mind: 'an error message', identity: {id: 1}});
-			expect(flash.setMessage).toHaveBeenCalledWith('an error message');
-			expect($scope.flash).toBeDefined();
-			//expect($scope.flash.getMessage()).toBe('an error message');
-	  });
+	it("Should push an error message in flash messenger if $scope.mind is not an object", function(done) {
+		$rootScope.$on('$routeChangeSuccess', function () {
+			expect($scope.flash.getMessage()).toBe('an error message');
+			return done();
+		});
+
+		EditController = $controller('EditMindCtrl', { $scope: $scope, mind: 'an error message', identity: {id: 1}});
+		//The message will be available when $routeChangeSuccess event is triggered
+		$rootScope.$broadcast('$routeChangeSuccess', {});
+	});
 });
 
 
