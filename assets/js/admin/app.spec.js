502f1c321f45a8f16aea45f2af0e9a27784074ac
devitito <julien_desbois@hotmail.com>
2014-12-17 17:32:37 +0100

    bug fix

--- assets/js/admin/app.spec.js
+++ assets/js/admin/app.spec.js
@@ -5,13 +5,14 @@
 describe("admin routes", function() {
 	beforeEach(module('mindmeteo'));
 
-	var $location, $route, $rootScope, $q, $httpBackend;;
+	var $location, $route, $rootScope, $q, $httpBackend, $injector;
 
-	beforeEach(inject(function(_$location_, _$route_, _$rootScope_, _$q_, _$httpBackend_){
+	beforeEach(inject(function(_$location_, _$route_, _$rootScope_, _$q_, _$httpBackend_, _$injector_){
 		$location = _$location_;
 		$route = _$route_;
 		$rootScope = _$rootScope_;
 		$q = _$q_;
+		$injector = _$injector_;
 		$httpBackend = _$httpBackend_;
 		$httpBackend.expectGET('/csrfToken').respond(200);
 	}));
@@ -23,8 +24,12 @@ describe("admin routes", function() {
 			function(_mindFactory_, _identityService_) {
 				$httpBackend.expectGET('/js/admin/partials/mind/edit.html').respond(200);
 
+				var mind = $q.defer();
+				mind.resolve('a mind');
 				mindFactory = _mindFactory_;
-				spyOn(mindFactory, 'get').and.returnValue('a mind');
+				spyOn(mindFactory, 'get').and.callFake(function (id, success, error) {
+					return success(mind.promise);
+				});
 
 				var identity = $q.defer();
 				identity.resolve('an identity');
@@ -33,7 +38,9 @@ describe("admin routes", function() {
 			}
 		));
 
-		it('should resolve mind and identity', inject(function($injector) {
+		it('should resolve mind and identity', function() {
+			var resolvedMind;
+			var resolvedIdentity;
 			expect($location.path()).toBe( '' );
 			$location.path('/minds/edit/2');
 
@@ -45,14 +52,20 @@ describe("admin routes", function() {
 
 			// We need to do $injector.invoke to resolve dependencies
 			var mindPromise = $injector.invoke($route.current.resolve.mind);
-			mindPromise.then(function() {
-				expect($route.current.resolve.mind).toBe('a mind');
+			mindPromise.then(function(value) {
+				resolvedMind = value;
 			});
 
 			var identityPromise = $injector.invoke($route.current.resolve.identity);
-			identityPromise.then(function() {
-				expect($route.current.resolve.identity).toBe('an identity');
+			identityPromise.then(function(value) {
+				resolvedIdentity = value;
 			});
-    }));
+
+			// Propagate promise resolution to 'then' functions using $apply().
+			$rootScope.$apply();
+			expect(resolvedMind).toBe('a mind');
+			expect(resolvedIdentity).toBe('an identity');
+
+    });
 	});
 });
