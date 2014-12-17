7cdb63c7291bd30e04f1f9f1d2869ace13f5b3aa
devitito <julien_desbois@hotmail.com>
2014-12-17 11:36:01 +0100

    mind edit controller test: if mind is fetched correctly

--- assets/js/admin/Controller/Mind/EditController.spec.js
+++ assets/js/admin/Controller/Mind/EditController.spec.js
@@ -33,6 +33,17 @@ describe("Mind.EditController", function() {
 		//The message will be available when $routeChangeSuccess event is triggered
 		$rootScope.$broadcast('$routeChangeSuccess', {});
 	});
+
+	it("Shouldn't push an error message in flash messenger if $scope.mind is an object", function(done) {
+		$rootScope.$on('$routeChangeSuccess', function () {
+			expect($scope.flash).toBeUndefined();
+			return done();
+		});
+
+		EditController = $controller('EditMindCtrl', { $scope: $scope, mind: {id:1, name: 'a-name', email:'anemail@domain.ext'}, identity: {id: 1}});
+		//$routeChangeSuccess is normaly triggered when view and controller are loaded
+		$rootScope.$broadcast('$routeChangeSuccess', {});
+	});
 });
 
 
