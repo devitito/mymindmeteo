7798e533af510eada5c53c63342756de105bdb67
devitito <julien_desbois@hotmail.com>
2014-12-19 16:41:58 +0100

    grunt test task

---
+++ tasks/config/mocha.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+	grunt.config.set('mochaTest', {
+		test: {
+			options: {
+				reporter: 'spec'
+			},
+			src: ['tests/**/*.spec.js']
+		}
+	});
+	grunt.loadNpmTasks('grunt-mocha-test');
+};
