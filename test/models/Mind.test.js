0454a17affbbf2f03ddfa96523c62d59c03c252f
devitito <julien_desbois@hotmail.com>
2015-01-13 16:20:29 +0100

    dummy mind model test

---
+++ test/models/Mind.test.js
@@ -0,0 +1,13 @@
+require('should');
+
+describe('Mind', function() {
+
+  /*it ('should have the same amount of documents as in fixtures', function(done) {
+		Mind.find().exec(function(err, minds) {
+			//fixtures['oranges'].length.should.be.greaterThan(0);
+			minds.length.should.be.eql(2);
+			done();
+		});
+	});*/
+
+});
