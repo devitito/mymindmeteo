0925a6a8cf34b6870393842f4bf01921c99736cf
devitito <julien_desbois@hotmail.com>
2015-01-13 16:18:47 +0100

    login test helper

---
+++ test/helpers/login.js
@@ -0,0 +1,45 @@
+var superagent = require('superagent');
+var promise = require('bluebird');
+var agent = superagent.agent();
+var admin = {
+  "nameoremail": "admin",
+  "password": "adminadmin"
+};
+var demo = {
+  "nameoremail": "demo",
+  "password": "demodemo"
+};
+
+exports.admin = function (request, done) {
+  var deferred = promise.defer();
+	request
+    .post('/session/create')
+    .send(admin)
+    .end(function (err, res) {
+      if (err) {
+				deferred.reject(err);
+        throw err;
+      }
+      agent.saveCookies(res);
+			deferred.resolve(agent);
+      if (done != undefined) done(agent);
+    });
+	return deferred.promise;
+};
+
+exports.demo = function (request, done) {
+  var deferred = promise.defer();
+	request
+    .post('/session/create')
+    .send(demo)
+    .end(function (err, res) {
+      if (err) {
+				deferred.reject(err);
+        throw err;
+      }
+      agent.saveCookies(res);
+			deferred.resolve(agent);
+      if (done != undefined) done(agent);
+    });
+	return deferred.promise;
+};
