93b28a7751692558fb88c32f7323cc0e4076eb37
devitito <julien_desbois@hotmail.com>
2015-01-14 16:51:59 +0100

    db creation script errors

--- test/mysql_add_user.sql
+++ test/mysql_add_user.sql
@@ -1,7 +1,7 @@
 CREATE USER 'mymeteo_app'@'%' IDENTIFIED BY 'supersecret';
 GRANT ALL PRIVILEGES ON *.* TO 'mymeteo_app'@'%' WITH GRANT OPTION;
 
-CREATE TABLE mymeteo.minds (
+CREATE TABLE mymeteo_test.minds (
   id varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   name varchar(64) COLLATE utf8_unicode_ci NOT NULL,
   email varchar(128) COLLATE utf8_unicode_ci NOT NULL,
@@ -15,5 +15,5 @@ CREATE TABLE mymeteo.minds (
   UNIQUE KEY email (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO mymeteo.minds ( id, name, email, password, joindate, timezone, locale, role ) VALUES ( '1', 'demo', 'demo@mindmeteo.com', '$2a$10$TLlpkWTS.7g8drPVwJMNeeLJ.sMIGJEbobkGi7lMM018awn7N2Z7O', '2015-01-12T00:00:00.000Z', 'Europe/Paris', 'fr_FR', 'demo');
-INSERT INTO mymeteo.minds ( id, name, email, password, joindate, timezone, locale, role ) VALUES ( '2', 'admin', 'admin@mindmeteo.com', '$2a$10$OXxj/V2urwpGMr6KeeNzI..pyCKTC8MxKu/XgnB4GEA13wpN3eOva', '2015-01-12T00:00:00.000Z', 'Europe/Paris', 'fr_FR', 'admin');
+INSERT INTO mymeteo_test.minds ( id, name, email, password, joindate, timezone, locale, role ) VALUES ( '1', 'demo', 'demo@mindmeteo.com', '$2a$10$TLlpkWTS.7g8drPVwJMNeeLJ.sMIGJEbobkGi7lMM018awn7N2Z7O', '2015-01-12T00:00:00.000Z', 'Europe/Paris', 'fr_FR', 'demo');
+INSERT INTO mymeteo_test.minds ( id, name, email, password, joindate, timezone, locale, role ) VALUES ( '2', 'admin', 'admin@mindmeteo.com', '$2a$10$OXxj/V2urwpGMr6KeeNzI..pyCKTC8MxKu/XgnB4GEA13wpN3eOva', '2015-01-12T00:00:00.000Z', 'Europe/Paris', 'fr_FR', 'admin');
