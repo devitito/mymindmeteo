9e6cf15784b439150985a8932873ae760a112ab2
devitito <julien_desbois@hotmail.com>
2015-01-19 17:15:35 +0100

    aws cookbook

---
+++ resources/cookbooks/aws/metadata.rb
@@ -0,0 +1,9 @@
+name             'aws'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures aws'
+long_description 'Installs/Configures aws'
+version          '0.1.0'
+
+depends 'awscli' , '~> 1.0.1'
