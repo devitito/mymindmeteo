90930fb71c988a45b3fcbd1a9ab96fa4375b84cf
devitito <julien_desbois@hotmail.com>
2015-01-19 18:02:54 +0100

    install ruby 2.0

--- resources/cookbooks/ruby/recipes/default.rb
+++ resources/cookbooks/ruby/recipes/default.rb
@@ -7,7 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
-execute "install ruby and gem" do
-  command "apt-get install zlib1g-dev libopenssl-ruby1.9.1 libssl-dev libyaml-0-2 libxslt-dev libxml2-dev libreadline-gplv2-dev libncurses5-dev file ruby2.0-dev git --yes --fix-missing"
+execute "install ruby-install" do
+  command "wget -O ruby-install-0.5.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.5.0.tar.gz"
+	command "tar -xzvf ruby-install-0.5.0.tar.gz"
+	command "cd ruby-install-0.5.0/"
+	command "make install"
+	action :run
+end
+
+execute "install ruby 2.0 into /usr/local" do
+  command "ruby-install --system ruby 2.0.0-p481"
+	action :run
+end
+
+execute "install gem" do
+  command "apt-get install zlib1g-dev libopenssl-ruby1.9.1 libssl-dev libyaml-0-2 libxslt-dev libxml2-dev libreadline-gplv2-dev libncurses5-dev file git --yes --fix-missing"
   action :run
 end
