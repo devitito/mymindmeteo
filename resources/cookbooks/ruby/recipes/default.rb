9725adbef6081c9cd2b062dbb218334da3bdb9c0
devitito <julien_desbois@hotmail.com>
2015-01-19 18:30:37 +0100

    ruby-install

--- resources/cookbooks/ruby/recipes/default.rb
+++ resources/cookbooks/ruby/recipes/default.rb
@@ -7,21 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-execute "download ruby-install" do
-  command "wget -O ruby-install-0.5.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.5.0.tar.gz"
-	action :run
-end
-
-execute "unpack ruby-install" do
-	command "tar -xzvf ruby-install-0.5.0.tar.gz"
-	action :run
-end
-
-execute "install ruby-install" do
-	command "cd ruby-install-0.5.0/"
-	command "make install"
-	action :run
-end
+include_recipe 'ruby_install::default'
 
 execute "install ruby 2.0 into /usr/local" do
   command "ruby-install --system ruby 2.0.0-p481"
