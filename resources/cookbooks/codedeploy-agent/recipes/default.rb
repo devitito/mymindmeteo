919b19f3cd17cd2f254cadfb0e8201bec7bb8c20
devitito <julien_desbois@hotmail.com>
2015-01-19 15:55:31 +0100

    .deb for codedeploy_aganty

--- resources/cookbooks/codedeploy-agent/recipes/default.rb
+++ resources/cookbooks/codedeploy-agent/recipes/default.rb
@@ -1,10 +1,10 @@
-remote_file "#{Chef::Config[:file_cache_path]}/codedeploy-agent.rpm" do
-    source "https://s3.amazonaws.com/aws-codedeploy-us-east-1/latest/codedeploy-agent.noarch.rpm"
+remote_file "#{Chef::Config[:file_cache_path]}/codedeploy-agent_all.deb" do
+    source "https://s3.amazonaws.com/aws-codedeploy-us-east-1/latest/codedeploy-agent_all.deb"
 end
 
 package "codedeploy-agent" do
 	action :install
-	source "#{Chef::Config[:file_cache_path]}/codedeploy-agent.rpm"
+	source "#{Chef::Config[:file_cache_path]}/codedeploy-agent_all.deb"
 end
 
 service "codedeploy-agent" do
