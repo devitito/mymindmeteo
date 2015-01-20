a136308f7ede295d2fa3cd7a5a35867072d9f63f
devitito <julien_desbois@hotmail.com>
2015-01-20 12:30:22 +0100

    use chef file cache

--- resources/cookbooks/aws/recipes/codedeploy-agent.rb
+++ resources/cookbooks/aws/recipes/codedeploy-agent.rb
@@ -1,12 +1,12 @@
 execute "download codedeploy-agent" do
-	command "aws s3 cp s3://aws-codedeploy-us-east-1/latest/install /home/ubuntu/ --region us-east-1"
+	command "aws s3 cp s3://aws-codedeploy-us-east-1/latest/install #{Chef::Config[:file_cache_path]} --region us-east-1"
   action :run
 end
 
 execute "Install codedeploy-agent" do
-	command "chmod +x /home/ubuntu/install"
-	command "sed -i 's/sleep(.*)/sleep(10)/' install"
+	command "chmod +x #{Chef::Config[:file_cache_path]}/install"
+	command "sed -i 's/sleep(.*)/sleep(10)/' #{Chef::Config[:file_cache_path]}/install"
   command "apt-get -f install"
-  command "/home/ubuntu/install auto"
+  command "#{Chef::Config[:file_cache_path]}/install auto"
   action :run
 end
