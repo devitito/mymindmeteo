2628b311e8ebdf50f4f46725c4094ced31a99097
devitito <julien_desbois@hotmail.com>
2015-01-20 12:32:21 +0100

    split for debug

--- resources/cookbooks/aws/recipes/codedeploy-agent.rb
+++ resources/cookbooks/aws/recipes/codedeploy-agent.rb
@@ -3,8 +3,13 @@ execute "download codedeploy-agent" do
   action :run
 end
 
-execute "Install codedeploy-agent" do
+execute "chmod codedeploy-agent install file" do
 	command "chmod +x #{Chef::Config[:file_cache_path]}/install"
+  action :run
+end
+
+
+execute "Install codedeploy-agent" do
 	command "sed -i 's/sleep(.*)/sleep(10)/' #{Chef::Config[:file_cache_path]}/install"
   command "apt-get -f install"
   command "#{Chef::Config[:file_cache_path]}/install auto"
