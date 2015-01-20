94f37feb33bde2f5351d3dfae468f7635a49e7e0
devitito <julien_desbois@hotmail.com>
2015-01-20 14:48:24 +0100

    code deploy instructions from amazone

--- resources/cookbooks/aws/recipes/codedeploy-agent.rb
+++ resources/cookbooks/aws/recipes/codedeploy-agent.rb
@@ -27,3 +27,9 @@ execute "Install codedeploy-agent" do
   command "dpkg -i #{Chef::Config[:file_cache_path]}/codedeploy-agent_all.deb"
   action :run
 end
+
+#sudo apt-get install ruby2.0
+#cd /home/ubuntu
+#sudo aws s3 cp s3://aws-codedeploy-us-east-1/latest/install . --region us-east-1
+#sudo chmod +x ./install
+#sudo ./install auto
