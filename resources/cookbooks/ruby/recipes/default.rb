72f7e38b703fe873ab7ba7bcd542c216ffe57409
devitito <julien_desbois@hotmail.com>
2015-01-20 12:42:05 +0100

    ruby install

--- resources/cookbooks/ruby/recipes/default.rb
+++ resources/cookbooks/ruby/recipes/default.rb
@@ -7,10 +7,21 @@
 # All rights reserved - Do Not Redistribute
 #
 
+
+#
+# Cookbook Name:: ruby
+# Recipe:: default
+#
+# Copyright (C) 2014 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
 include_recipe 'ruby_install::default'
 
 execute "install ruby 2.0" do
-  command "ruby-install -M https://ftp.ruby-lang.org/pub/ruby ruby 2.0.0-p576"
+	command "ruby-install --rubies-dir /opt/rubies/ ruby 2.0.0-p576"
+  #command "ruby-install -M https://ftp.ruby-lang.org/pub/ruby ruby 2.0.0-p576"
 	command "ln -s /opt/rubies/ruby-2.0.0-p576/bin/ruby /usr/bin/ruby2.0"
 	action :run
 end
@@ -24,3 +35,8 @@ execute "install gem" do
   command "apt-get install zlib1g-dev libopenssl-ruby1.9.1 libssl-dev libyaml-0-2 libxslt-dev libxml2-dev libreadline-gplv2-dev libncurses5-dev file git --yes --fix-missing"
   action :run
 end
+
+#execute "install ruby 1.9.1" do
+#  command "apt-get install zlib1g-dev libopenssl-ruby1.9.1 libssl-dev libyaml-0-2 libxslt-dev libxml2-dev libreadline-gplv2-dev libncurses5-dev file ruby1.9.1-dev git --yes --fix-missing"
+#  action :run
+#end
