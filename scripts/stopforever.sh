a2c312bdaa468f053e085c90a2eee7f68b0bb26b
devitito <julien_desbois@hotmail.com>
2015-01-22 16:11:25 +0100

    log

--- scripts/stopforever.sh
+++ scripts/stopforever.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+forever list > /var/log/stopforever.out 2>&1
 if [ -f /home/ubuntu/mindmeteo/app.js]
 then 
-	forever stop /home/ubuntu/mindmeteo/app.js > /var/log/stopforever.out 2>&1
+	forever stop /home/ubuntu/mindmeteo/app.js >> /var/log/stopforever.out 2>&1
 else
-	echo '/home/ubuntu/mindmeteo/app.js not found. forever stop not called' > /var/log/stopforever.out 2>&1
+	echo '/home/ubuntu/mindmeteo/app.js not found. forever stop not called' >> /var/log/stopforever.out 2>&1
 fi
