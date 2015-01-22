601923d74239c1866b444c3fd8d47de173777a0b
devitito <julien_desbois@hotmail.com>
2015-01-22 16:31:48 +0100

    script fix

--- scripts/stopforever.sh
+++ scripts/stopforever.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 forever list > /var/log/stopforever.out 2>&1
-if [ -f /home/ubuntu/mindmeteo/app.js]
+if [ -f /home/ubuntu/mindmeteo/app.js ]
 then 
 	forever stop /home/ubuntu/mindmeteo/app.js >> /var/log/stopforever.out 2>&1
 else
