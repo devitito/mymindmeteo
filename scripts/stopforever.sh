6e296aabe387ca0a05c0a670f677d10e9cbde628
devitito <julien_desbois@hotmail.com>
2015-01-23 18:16:30 +0100

    exit code forced to 0

--- scripts/stopforever.sh
+++ scripts/stopforever.sh
@@ -2,7 +2,9 @@
 /usr/bin/forever list > /var/log/stopforever.out 2>&1
 if [ -e /home/ubuntu/mindmeteo/app.js ]
 then 
+  if
 	/usr/bin/forever stop /home/ubuntu/mindmeteo/app.js >> /var/log/stopforever.out 2>&1
+	exit 0
 else
 	echo '/home/ubuntu/mindmeteo/app.js not found. forever stop not called' >> /var/log/stopforever.out 2>&1
 fi
