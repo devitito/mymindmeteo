707ea832efb882f3dff8dc16deefed6cd273b370
devitito <julien_desbois@hotmail.com>
2015-01-26 13:10:30 +0100

    test per hour request

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -47,35 +47,18 @@ module.exports = {
 					cb(null, {'0': 5, '1': 2, '2': 5, '3': 9, '4': 5, '5':7, '6':6})
 				},
 				'testPerHour' : function (cb) {
-					cb(null, {
-					'0': 0, '1': 0, '2': 0, '3': 0, '4':0, '5':0, '6':0, '7': 0, '8':2, '9':35, '10': 56, '11':78, '12':61, '13':42, '14':69, '15':84, '16':89, '17':93, '18': 87, '19':54, '20':23, '21':13, '22':1,'23':0})
+					ElasticService.request('test-per-hour', {})
+					.then(function (reqRes) {
+						cb(null, reqRes);
+					}).catch(function (err) {
+						cb(err);
+					});
 				}
 			},
 			function (err, results) {
 				if (err) return next(err);
 				else res.json(results);
 			});
-
-//		ElasticService.request('sensor-per-topic', {}, function graphValues(err, values) {
-	//		if (err) return next(err);
-		/*	res.json({
-				'sensorPerTopic' : {'health': 10, 'love': 12, 'money': 11},
-				'mostPopularSensor' : [
-					{'1' : ['How is your caca?', '1021']},
-					{'2' : ['Did you run out of toilet paper today?', '543']},
-					{'3' : ['Did you quit your job today?', '521']},
-					{'4' : ['What\'s your plan for tonight?', '328']},
-					{'5' : ['Did you score yesterday?', '196']}
-				],
-				'testPerDay' : {'0': 5, '1': 2, '2': 5, '3': 9, '4': 5, '5':7, '6':6},
-				'testPerHour' : {
-					'0': 0, '1': 0, '2': 0, '3': 0, '4':0, '5':0, '6':0, '7': 0, '8':2, '9':35, '10': 56, '11':78, '12':61, '13':42, '14':69, '15':84, '16':89, '17':93, '18': 87, '19':54, '20':23, '21':13, '22':1,'23':0}
-			});*/
-
-
-
-
-	//	});
 	}
 };
 
---
+++ api/services/requests/test-per-hour.js
@@ -0,0 +1,34 @@
+/**
+ * @project Mind meteo
+ * @author devitito
+ * @date
+ *
+ */
+
+module.exports.query = function(options) {
+	return {
+		index: 'mindmeteo',
+		type: 'records',
+		trackScores: false,
+		body: {
+			"aggs": {
+				"records_by_hour": {
+					"terms": {
+						"field": "hour",
+						"order" : { "_term" : "asc" }
+      		}
+    		},
+			}
+		}
+	};
+};
+
+module.exports.parse = function(result) {
+	var res = [];
+
+	result.aggregations.records_by_hour.buckets.forEach(function(entry) {
+		res[eval(entry.key)] = entry.doc_count;
+		//eval("res."+entry.key+" = " + entry.doc_count);
+	});
+	return res;
+};
