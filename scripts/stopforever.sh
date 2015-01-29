81e217ab96f9ad8cc65b6b9c08032f35ad2eac1d
devitito <julien_desbois@hotmail.com>
2015-01-29 18:45:26 +0100

    stop forever script error syntax

--- scripts/stopforever.sh
+++ scripts/stopforever.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 /usr/bin/forever list > /var/log/stopforever.out 2>&1
 if [ -e /home/ubuntu/mindmeteo/app.js ]
-then 
-  if
+then
 	/usr/bin/forever stop /home/ubuntu/mindmeteo/app.js >> /var/log/stopforever.out 2>&1 || true
 else
 	echo '/home/ubuntu/mindmeteo/app.js not found. forever stop not called' >> /var/log/stopforever.out 2>&1
