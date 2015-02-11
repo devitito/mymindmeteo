8df560cfad8969ef6269cad40fdc73af9e6589b9
devitito <julien_desbois@hotmail.com>
2015-02-11 09:59:53 +0100

    mind dashboard controller split

---
+++ assets/js/Services/climateChart.js
@@ -0,0 +1,95 @@
+/**
+ * @project Mind meteo
+ * @author devitito
+ * @date
+ *
+ */
+
+mindServices.factory("climateChartHelper", function() {
+	var factory = {};
+
+	factory.load = function ($scope, climat) {
+	try {
+		if (climat.error) {
+			$scope.message = climat.error;
+			$scope.total = '-';
+			$scope.sunny = '-';
+			$scope.rainy = '-';
+		}
+		else if (!climat.data.length) {
+			$scope.message = 'No meteo data captured.';
+			$scope.total = climat.info.total;
+			$scope.sunny = climat.info.sunny;
+			$scope.rainy = climat.info.rainy;
+		}
+		else {
+			var climate = {};
+			climate.options = {
+				'is3D':true,
+				fontSize: 14,
+				colors : [/*'#FF0000',*/ '#00ADEF'/*, '#85bb65', 'yellow'*/],
+				legend : {position: 'none'},
+				chartarea: {left:0,top:0,width:'100%',height:'100%'},
+				pointSize : 4,
+				curveType: 'function',
+				'hAxis': {
+					format: 'yyyy-MM-dd',
+					slantedText: false,
+					textPosition : 'none',
+					viewWindowMode: 'maximized',
+				},
+				vAxis : {
+					//gridlines: { count : 2},
+					//ticks: [{v:-10, f:'Devastation'}, {v:-5, f:'Wind and rain'}, {v:0, f:'Not sunny Not raining'}, {v:5, f:'Spring impression'}, {v:10, f:'T shirt and bermuda'}],
+					ticks: [{v:-10, f:':(('}, {v:-5, f:':('}, {v:0, f:':|'}, {v:5, f:':)'}, {v:10, f:':))'}],
+					maxValue : 10,
+					minValue : -10,
+					//textPosition : 'in',
+				},
+			};
+			climate.type = 'LineChart';
+			climate.data = convertToDataTable(climat.data);
+			$scope.climate = climate;
+
+			$scope.total = climat.info.total;
+			$scope.sunny = climat.info.sunny;
+			$scope.rainy = climat.info.rainy;
+		};
+	} catch (e) {
+		$scope.message = "We couldn't retrieve your climate data. Please try again";
+	};
+};
+
+var convertToDataTable = function (json){
+	var d= {};
+	d.rows = json.map(function(item){
+		return {
+			"c": [{
+				"v": moment(item.date).format('YYYY-MMM-D')
+			},
+							/*	{
+        			"v": item.love
+        		},
+        		{
+        			"v": item.health
+        		},
+						{
+        			"v": item.money
+        		},*/
+						{
+							"v": item.mood
+        		}]
+		}
+	});
+	d.cols =  [{ label: "Days", type: "string"},
+						 /*{ label: "Love", type: "number"},
+					{ l	abel: "Health", type: "number"},
+					{ label: "Money", type: "number"},*/
+						 { label: "Mood", type: "number"}, ];
+	d.p = null;
+
+	return d;
+};
+
+	return factory;
+});
--- assets/js/mind/Controller/DashboardController.js
+++ assets/js/mind/Controller/DashboardController.js
@@ -5,8 +5,8 @@
  *
  */
 
-var mindDashboardCtrl = mindControllers.controller('mindDashboardCtrl', ['$scope', '$location', '$modal', '$filter', 'identity', 'flash', 'sessionFactory', 'climat', 'moment', 'ngTableParams', 'statementsFactory', 'statsFactory', 'recordsFactory',
-    function ($scope, $location, $modal, $filter, identity, flash, sessionFactory, climat, moment, ngTableParams, statementsFactory, statsFactory, recordsFactory) {
+var mindDashboardCtrl = mindControllers.controller('mindDashboardCtrl', ['$scope', '$location', '$modal', '$filter', 'identity', 'flash', 'sessionFactory', 'climat', /*'moment',*/ 'ngTableParams', 'statementsFactory', 'statsFactory', 'recordsFactory', 'climateChartHelper',
+    function ($scope, $location, $modal, $filter, identity, flash, sessionFactory, climat, /*moment,*/ ngTableParams, statementsFactory, statsFactory, recordsFactory, climateChartHelper) {
 			$scope.go = function (url) {
 				$location.path(url);
 			};
@@ -52,7 +52,8 @@ var mindDashboardCtrl = mindControllers.controller('mindDashboardCtrl', ['$scope
 							//update the climate
 							statsFactory.climate($scope.identity.name)
 							.then(function (climat) {
-								loadClimateChart(climat);
+								climateChartHelper.load($scope, climat);
+								//loadClimateChart(climat);
 								//Generate the reports
 								statementsFactory.generate({id: $scope.identity.id}).$promise
 								.then(function (reports) {
@@ -101,90 +102,6 @@ var mindDashboardCtrl = mindControllers.controller('mindDashboardCtrl', ['$scope
 				}
 			});
 
-			var convertToDataTable = function (json){
-      	var d= {};
-      	d.rows = json.map(function(item){
-      	  return {
-        		"c": [{
-        			"v": moment(item.date).format('YYYY-MMM-D')
-        		},
-        	/*	{
-        			"v": item.love
-        		},
-        		{
-        			"v": item.health
-        		},
-						{
-        			"v": item.money
-        		},*/
-						{
-        			"v": item.mood
-        		}]
-        	}
-      	});
-      	d.cols =  [{ label: "Days", type: "string"},
-					/*{ label: "Love", type: "number"},
-					{ label: "Health", type: "number"},
-					{ label: "Money", type: "number"},*/
-					{ label: "Mood", type: "number"}, ];
-      	d.p = null;
-
-        return d;
-     }
-
-			var loadClimateChart = function(climat) {
-				try {
-				if (climat.error) {
-					$scope.message = climat.error;
-					$scope.total = '-';
-					$scope.sunny = '-';
-					$scope.rainy = '-';
-				}
-				else if (!climat.data.length) {
-					$scope.message = 'No meteo data captured.';
-					$scope.total = climat.info.total;
-					$scope.sunny = climat.info.sunny;
-					$scope.rainy = climat.info.rainy;
-				}
-				else {
-					var climate = {};
-					climate.options = {
-						'is3D':true,
-						fontSize: 14,
-						colors : [/*'#FF0000',*/ '#00ADEF'/*, '#85bb65', 'yellow'*/],
-						legend : {position: 'none'},
-						chartarea: {left:0,top:0,width:'100%',height:'100%'},
-						pointSize : 4,
-						curveType: 'function',
-						'hAxis': {
-							format: 'yyyy-MM-dd',
-							slantedText: false,
-							textPosition : 'none',
-							viewWindowMode: 'maximized',
-
-						},
-						vAxis : {
-							//gridlines: { count : 2},
-							//ticks: [{v:-10, f:'Devastation'}, {v:-5, f:'Wind and rain'}, {v:0, f:'Not sunny Not raining'}, {v:5, f:'Spring impression'}, {v:10, f:'T shirt and bermuda'}],
-							ticks: [{v:-10, f:':(('}, {v:-5, f:':('}, {v:0, f:':|'}, {v:5, f:':)'}, {v:10, f:':))'}],
-							maxValue : 10,
-							minValue : -10,
-							//textPosition : 'in',
-						},
-					};
-					climate.type = 'LineChart';
-					climate.data = convertToDataTable(climat.data);
-					$scope.climate = climate;
-
-					$scope.total = climat.info.total;
-					$scope.sunny = climat.info.sunny;
-					$scope.rainy = climat.info.rainy;
-				};
-			} catch (e) {
-				$scope.message = "We couldn't retrieve your climate data. Please try again";
-			};
-			};
-
 			$scope.processing = false;
-			loadClimateChart(climat);
+			climateChartHelper.load($scope, climat);
 }]);
