ea214ff489c48f6eccc8cc84e6c3a00c1597a578
devitito <julien_desbois@hotmail.com>
2015-02-11 09:31:56 +0100

    split StatementGenerator

--- api/services/StatementGenerator.js
+++ api/services/StatementGenerator.js
@@ -12,6 +12,28 @@ function randomIntFromInterval(min,max)
 	return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+var save = function (deferred, statement) {
+	Statement.create(statement)
+	.then(function(createdStatement) {
+		deferred.resolve(createdStatement);
+	})
+	.catch(function(err) {
+		deferred.reject(err);
+	});
+};
+
+var processTemplate = function(deferred, report, mindid) {
+	console.log(report.template);
+	//todo: transform template into plain text
+	var statement = {
+		body: report.template,
+		notread: true,
+		mind: mindid,
+		report: report.id
+	};
+	return statement;
+};
+
 module.exports.generate = function(mindid) {
 	var deferred = promise.defer();
 
@@ -24,21 +46,8 @@ module.exports.generate = function(mindid) {
 			//pick a random report from this list and generate statement
 			Report.findOneById(randomIntFromInterval(1, count+1))
 			.then(function (report) {
-				console.log(report.template);
-				//todo: transform template into plain text
-				var statement = {
-					body: report.template,
-					notread: true,
-					mind: mindid,
-					report: report.id
-				};
-				Statement.create(statement)
-				.then(function(createdStatement) {
-					deferred.resolve(createdStatement);
-				})
-				.catch(function(err) {
-					deferred.reject(err);
-				});
+				var statement = processTemplate(deferred, report, mindid);
+				save(deferred, statement);
 			});
 		});
 	})
