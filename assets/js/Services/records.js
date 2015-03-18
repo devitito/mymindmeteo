8cac332a40035d16e851bc73ba7ae4371d2c177c
devitito <julien_desbois@hotmail.com>
2015-03-18 18:08:18 +0100

    cosmetics

--- assets/js/Services/records.js
+++ assets/js/Services/records.js
@@ -5,8 +5,8 @@
  *
  */
 
-mindServices.factory('recordsFactory', ['$resource', '$q', /*'$modal', 'statementsFactory', 'statsFactory', 'climateChartHelper', 'sensorsFactory',*/
-	function($resource, $q/*, $modal, statementsFactory, statsFactory, climateChartHelper, sensorsFactory*/){
+mindServices.factory('recordsFactory', ['$resource', '$q',
+	function($resource, $q){
 	var factory = {};
 	var resource = $resource('/record/:id', {id:'@id'}, {
 		saveBulk: {method: 'POST', url: '/record/saveBulk'}
@@ -24,53 +24,6 @@ mindServices.factory('recordsFactory', ['$resource', '$q', /*'$modal', 'statemen
 		//deferred.resolve('ok');
 		return deferred.promise;
 	}
-
-	/*factory.launch = function (scope) {
-		var modalInstance = $modal.open({
-			templateUrl: '/js/components/modals/record/record.html',
-			controller: 'RecordModalCtrl',
-			resolve: {
-				identity: ['identityService', '$location', function(identityService, $location) {
-					var identityRequest = identityService.get();
-					identityRequest.catch(function(reason) {
-						$location.path('/');
-					});
-					return identityRequest;
-				}],
-				sensorList : ['sensorsFactory', function (sensorsFactory) {
-					return sensorsFactory.listBy({field: "status", value: "approved"});
-				}]
-			}
-		});
-
-		modalInstance.result
-		.then(function (records) {
-			if (angular.isUndefined(records))
-			{
-				scope.processing = false;
-			}
-			else
-			{
-				if (records.length) {
-					scope.processing = true;
-
-					factory.save({id:scope.identity.id}, records)
-					.then(function (success) {
-						//update the climate
-						statsFactory.climate(scope.identity.name)
-						.then(function (climat) {
-							climateChartHelper.load(scope, climat);
-							scope.processing = false;
-
-						});
-					})
-					.catch(function (err) {
-						scope.showError(err.data);
-					});
-				}
-			}
-		});
-	};*/
 
 	return factory;
 }]);
