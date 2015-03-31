d9488739ae0a1aa9b17f1d06fd24b3dd6c0187e4
devitito <the.devitito@gmail.com>
2015-03-31 12:52:41 +0200

    grunt debug

--- tasks/config/nodemon.js
+++ tasks/config/nodemon.js
@@ -7,7 +7,16 @@ module.exports = function(grunt) {
                 ignore: ['assets/bower_components/**', 'assets/coverage/**', '.git', 'README.md','.tmp/**', 'node_modules/**', 'test/**', 'resources/**', 'scripts/**', 'tasks/**', 'views/**'],
                 watchedExtensions: ['js'],
             }
-        }
+        },
+        debug: {
+            script: 'app.js',
+            options: {
+                env: {NODE_ENV: 'dev'},
+                ignore: ['assets/bower_components/**', 'assets/coverage/**', '.git', 'README.md','.tmp/**', 'node_modules/**', 'test/**', 'resources/**', 'scripts/**', 'tasks/**', 'views/**'],
+                watchedExtensions: ['js'],
+                exec: 'node-theseus'
+            },
+        },
     });
 	grunt.loadNpmTasks('grunt-nodemon');
 };
