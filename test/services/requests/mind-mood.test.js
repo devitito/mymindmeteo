814d0f396c660276ec0b26c55d21f8e13d89aed8
devitito <the.devitito@gmail.com>
2015-04-14 19:18:38 +0200

    mind-mood test

---
+++ test/services/requests/mind-mood.test.js
@@ -0,0 +1,50 @@
+/**
+ * @project Mind meteo
+ * @author devitito
+ * @date
+ *
+ */
+
+var sinon = require('sinon'),
+    should = require('should'),
+    moment = require('moment-timezone');
+
+describe('mind-mood ES request', function () {
+
+  describe("query() method", function() {
+    var clock, zone;
+
+    before(function() {
+      //Set server time zone
+      //moment.tz.setDefault('America/Los_Angeles');
+      moment.tz.setDefault('Europe/Paris');
+      //console.log(moment().zoneName());
+      //set server's current locale time to "2011-10-02 00:55:55"
+      clock = sinon.useFakeTimers(new Date(2011,9,2,00,55,55).getTime());
+    });
+
+    after(function() {
+      clock.restore();
+      moment.tz.setDefault(null);
+      //console.log(moment().zoneName());
+    });
+
+    it("should use mind's locale timezone to select the range of records", function(done) {
+      var adapter = require('../../../api/services/requests/mind-mood.js');
+      var query = adapter.query({id:1, timezone:'Europe/Helsinki'});
+
+      //console.log(moment().format());
+      //console.log(moment.utc().format());
+
+      should.exist(query);
+      should.exist(query.body.query.filtered.filter.range.tstamp.gte);
+      query.body.query.filtered.filter.range.tstamp.gte.should.be.equal("2011-10-02 00:00:00");
+
+      var query = adapter.query({id:1, timezone:'Europe/London'});
+      query.body.query.filtered.filter.range.tstamp.gte.should.be.equal("2011-10-01 00:00:00");
+      done();
+    });
+
+  });
+
+});
