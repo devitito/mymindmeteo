5c78b8ad35c6a8e892d4bcb0eeb6a4edf8d5a946
devitito <the.devitito@gmail.com>
2015-04-15 13:04:18 +0200

    cosmetics

--- assets/js/modules/dashboard.js
+++ assets/js/modules/dashboard.js
@@ -40,112 +40,108 @@ angular.module('dashboard', ['ngResource', 'session', 'stats', 'climate', 'googl
 		}
 }])
 .controller('mindDashboardCtrl', ['$scope', '$location', '$timeout', '$window', 'identity', 'sessionFactory', 'statsFactory', 'climateChartHelper', 'tableHelper', 'usSpinnerService', 'reportCategories', 'emociconeService', 'reportRanges', 'snapRemote',
-    function ($scope, $location, $timeout, $window, identity, sessionFactory, statsFactory, climateChartHelper, tableHelper, usSpinnerService, reportCategories, emociconeService, reportRanges, snapRemote) {
-			$scope.go = function (url) {
-				$location.path(url);
-			};
+  function ($scope, $location, $timeout, $window, identity, sessionFactory, statsFactory, climateChartHelper, tableHelper, usSpinnerService, reportCategories, emociconeService, reportRanges, snapRemote) {
+    $scope.go = function (url) {
+      $location.path(url);
+    };
 
-			statsFactory.climate(identity.name)
-			.then(function (climate) {
-				climateChartHelper.load($scope, climate);
-				//wait a bit for the graph to display on slow device
-				$timeout(function() {
-					usSpinnerService.stop('spinner');
-					$scope.spinneroff = true;
-				}, 1000);
-			})
-			.catch(function (error) {
-				$scope.showError(error);
-			});
+    statsFactory.climate(identity.name)
+    .then(function (climate) {
+      climateChartHelper.load($scope, climate);
+      //wait a bit for the graph to display on slow device
+      $timeout(function() {
+        usSpinnerService.stop('spinner');
+        $scope.spinneroff = true;
+      }, 1000);
+    })
+    .catch(function (error) {
+      $scope.showError(error);
+    });
 
-			$scope.identity = identity;
+    $scope.identity = identity;
 
-			$scope.logout = function () {
-				sessionFactory.destroy()
-				.then(function(success) {
-					$window.location.href = '/';
-				})
-				.catch(function(error) {
-					//todo display error
-				});
-			};
+    $scope.logout = function () {
+      sessionFactory.destroy()
+      .then(function(success) {
+        $window.location.href = '/';
+      })
+      .catch(function(error) {
+        //todo display error
+      });
+    };
 
-			$scope.ViewStatement = function(statement) {
-				statement.notread = false;
-				//$scope.editedStatement = statement;
-              $scope.go('/statement/view/'+statement.id);
-			};
+    $scope.ViewStatement = function(statement) {
+      statement.notread = false;
+      //$scope.editedStatement = statement;
+      $scope.go('/statement/view/'+statement.id);
+    };
 
-			$scope.sendStatement = function() {
-				$scope.editedStatement = undefined;
-				$scope.sentConfirm = 'Message sent successfully!';
-				$timeout(function () {
-					$scope.sentConfirm = undefined;
-				}, 3000);
-			};
+    $scope.sendStatement = function() {
+      $scope.editedStatement = undefined;
+      $scope.sentConfirm = 'Message sent successfully!';
+      $timeout(function () {
+        $scope.sentConfirm = undefined;
+      }, 3000);
+    };
 
-			$scope.close = function() {
-				$scope.editedStatement = undefined;
-			};
+    $scope.showError = function (error) {
+      $scope.processing = false;
+      $scope.error = error;
+      $timeout(function() {
+        $scope.error = undefined;
+      }, 5000);
+    };
 
-			$scope.showError = function (error) {
-				$scope.processing = false;
-				$scope.error = error;
-				$timeout(function() {
-					$scope.error = undefined;
-				}, 5000);
-			};
+    $scope.record = function () {
+      snapRemote.close();
+      $location.path('climate/record');
+      //recordsFactory.launch($scope);
+    };
 
-			$scope.record = function () {
-				snapRemote.close();
-				$location.path('climate/record');
-				//recordsFactory.launch($scope);
-			};
+    $scope.generate = function () {
+      snapRemote.close();
+      $location.path('statement/new');
+    };
 
-			$scope.generate = function () {
-				snapRemote.close();
-				$location.path('statement/new');
-			};
-
-			$scope.openReportList = function () {
-				$scope.show = 'reports';
-				$scope.newReports = false;
-			};
-
+/*    $scope.openReportList = function () {
+      $scope.show = 'reports';
+      $scope.newReports = false;
+    };*/
+/*
 			$scope.openClimate = function () {
 				$scope.show = 'climate';
-			};
+			};*/
 
-			$scope.tableParams = tableHelper.new('statements-list', {id:$scope.identity.id});
+    $scope.tableParams = tableHelper.new('statements-list', {id:$scope.identity.id});
 
-			$scope.show = 'climate';
-			$scope.processing = false;
-			$scope.spinneroff = false;
+    //$scope.show = 'climate';
+    $scope.processing = false;
+    $scope.spinneroff = false;
 
-			$scope.getImgByCategory = function (category) {
-				var img = reportCategories.img(category);
-				console.log(img);
-				return img;
-				//return reportCategories.img(category);
-			};
+    $scope.getImgByCategory = function (category) {
+      var img = reportCategories.img(category);
+      console.log(img);
+      return img;
+      //return reportCategories.img(category);
+    };
 
-			$scope.getCategoryLabel = function(category) {
-				var label = reportCategories.label(category);
-				console.log(label);
-				return 'Category: ' + label;
-			};
+    $scope.getCategoryLabel = function(category) {
+      var label = reportCategories.label(category);
+      console.log(label);
+      return 'Category: ' + label;
+    };
 
-			$scope.getEmocicone = function (range) {
-				var img = emociconeService.range2img(range);
-				console.log(img);
-				return img;
-			};
+    $scope.getEmocicone = function (range) {
+      var img = emociconeService.range2img(range);
+      console.log(img);
+      return img;
+    };
 
-			$scope.getMoodRangeLabel = function(range) {
-				var label = reportRanges.label(range);
-				console.log(label);
-				return 'Mood: ' + label;
-			}
+    $scope.getMoodRangeLabel = function(range) {
+      var label = reportRanges.label(range);
+      console.log(label);
+      return 'Mood: ' + label;
+    };
 
 			//snapRemote.enable();
 			/*snapRemote.getSnapper().then(function(snapper) {
@@ -157,6 +153,21 @@ angular.module('dashboard', ['ngResource', 'session', 'stats', 'climate', 'googl
 					log('Drawer closed!');
 				});
   		});*/
+
+    $scope.generateButtonLabel = function () {
+      if ($window.innerWidth >= 570)
+        $scope.label = "Need a new weather report for your lover, family, friends, boss...?";
+      else
+        $scope.label = "New report!";
+    }
+
+    $($window).resize(function() {
+      $scope.$apply(function(){
+         $scope.generateButtonLabel();
+      });
+    });
+
+    $scope.generateButtonLabel();
 }])
 .factory('recovery', ['$resource',
   function($resource){
