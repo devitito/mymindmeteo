c950ab3144e75eca9191831895224784bd47b250
devitito <the.devitito@gmail.com>
2015-04-16 12:57:37 +0200

    curve lines according to screen size

--- assets/js/modules/climate.js
+++ assets/js/modules/climate.js
@@ -83,70 +83,81 @@ angular.module('climate', ['emocicones', 'session', 'record'])
     $scope.records = [];
     $scope.sensor = sensorList[sensorIndex]._source;
 }])
-.factory("climateChartHelper", ['emociconeService', function(emociconeService) {
-	var factory = {};
+.factory("climateChartHelper", ['emociconeService', '$window', function(emociconeService, $window) {
+  var factory = {};
 
-	factory.load = function ($scope, climat) {
-	try {
-		if (climat.error) {
-			$scope.message = climat.error;
-			$scope.total = '-';
-			$scope.sunny = '-';
-			$scope.rainy = '-';
-		}
-		else if (!climat.data.length) {
-			$scope.message = 'No meteo data captured.';
-			$scope.total = climat.info.total;
-			$scope.sunny = climat.info.sunny;
-			$scope.rainy = climat.info.rainy;
-		}
-		else {
-			var climate = {};
-			climate.options = {
-				'is3D':true,
-				fontSize: 14,
-				colors : [/*'#FF0000',*/ '#00ADEF'/*, '#85bb65', 'yellow'*/],
-				legend : {position: 'none'},
-				chartarea: {left:0,top:0,width:'100%',height:'100%'},
-				pointSize : 6,
-				lineWidth: 3,
-				curveType: 'function',
-				'hAxis': {
-					format: 'yyyy-MM-dd',
-					slantedText: false,
-					textPosition : 'none',
-					viewWindowMode: 'maximized',
+  factory.load = function ($scope, climat) {
+    try {
+      if (climat.error) {
+        $scope.message = climat.error;
+        $scope.total = '-';
+        $scope.sunny = '-';
+        $scope.rainy = '-';
+      }
+      else if (!climat.data.length) {
+        $scope.message = 'No meteo data captured.';
+        $scope.total = climat.info.total;
+        $scope.sunny = climat.info.sunny;
+        $scope.rainy = climat.info.rainy;
+      }
+      else {
+        var climate = {};
 
-				},
-				vAxis : {
-					//gridlines: { count : 2},
-					//ticks: [{v:-10, f:'Devastation'}, {v:-5, f:'Wind and rain'}, {v:0, f:'Not sunny Not raining'}, {v:5, f:'Spring impression'}, {v:10, f:'T shirt and bermuda'}],
-					ticks: [{v:-10, f:':(('}, {v:-5, f:':('}, {v:0, f:':|'}, {v:5, f:':)'}, {v:10, f:':))'}],
-					textStyle: { fontSize: 16, bold: true, color: '#808080'},
-					maxValue : 10,
-					minValue : -10,
-					//textPosition : 'in',
-                  gridlines: { color : 'transparent'},
-                  //baselineColor: 'transparent'
-				},
-			};
-			climate.type = 'LineChart';
-			climate.data = convertToDataTable(climat.data, $scope);
-			$scope.climate = climate;
+        var lineWidth, pointSize;
+        if ($window.innerWidth > 629) {
+          pointSize = 6;
+          lineWidth = 3;
+        }
+        else {
+          pointSize = 3;
+          lineWidth = 2;
+        }
 
-			$scope.total = climat.info.total;
-			$scope.sunny = climat.info.sunny;
-			$scope.rainy = climat.info.rainy;
-			$scope.message = undefined;
-		};
-	} catch (e) {
-		$scope.message = "We couldn't retrieve your climate data. Please try again";
-	};
+        climate.options = {
+          'is3D':true,
+          fontSize: 14,
+          colors : [/*'#FF0000',*/ '#00ADEF'/*, '#85bb65', 'yellow'*/],
+          legend : {position: 'none'},
+          chartarea: {left:0,top:0,width:'100%',height:'100%'},
+          pointSize : pointSize,
+          lineWidth: lineWidth,
+          curveType: 'function',
+          'hAxis': {
+            format: 'yyyy-MM-dd',
+            slantedText: false,
+            textPosition : 'none',
+            viewWindowMode: 'maximized',
+
+          },
+          vAxis : {
+            //gridlines: { count : 2},
+            //ticks: [{v:-10, f:'Devastation'}, {v:-5, f:'Wind and rain'}, {v:0, f:'Not sunny Not raining'}, {v:5, f:'Spring impression'}, {v:10, f:'T shirt and bermuda'}],
+            ticks: [{v:-10, f:':(('}, {v:-5, f:':('}, {v:0, f:':|'}, {v:5, f:':)'}, {v:10, f:':))'}],
+            textStyle: { fontSize: 16, bold: true, color: '#808080'},
+            maxValue : 10,
+            minValue : -10,
+            //textPosition : 'in',
+            gridlines: { color : 'transparent'},
+            //baselineColor: 'transparent'
+          },
+        };
+        climate.type = 'LineChart';
+        climate.data = convertToDataTable(climat.data, $scope);
+        $scope.climate = climate;
+
+        $scope.total = climat.info.total;
+        $scope.sunny = climat.info.sunny;
+        $scope.rainy = climat.info.rainy;
+        $scope.message = undefined;
+      };
+    } catch (e) {
+      $scope.message = "We couldn't retrieve your climate data. Please try again";
+    };
 };
 
-var convertToDataTable = function (json, $scope){
-	var d= {};
-	var newestItem = {};
+  var convertToDataTable = function (json, $scope){
+    var d= {};
+    var newestItem = {};
 	newestItem.date = moment('2014-06-01');
 	d.rows = json.map(function(item){
 		//Get the newest item in order to set the emocicones
@@ -179,7 +190,7 @@ var convertToDataTable = function (json, $scope){
 	d.p = null;
 
 	//Set the emocicones
-	$scope.emocicone = {};
+    $scope.emocicone = {};
 	$scope.emocicone.love = emociconeService.eval(newestItem.love);
 	$scope.emocicone.health = emociconeService.eval(newestItem.health);
 	$scope.emocicone.money = emociconeService.eval(newestItem.money);
@@ -187,7 +198,7 @@ var convertToDataTable = function (json, $scope){
 	return d;
 };
 
-	return factory;
+  return factory;
 }]);
 
 function randomIntFromInterval(min,max)
